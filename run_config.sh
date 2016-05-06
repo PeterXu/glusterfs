13a4bb18a6853e23fc3ae1df6faf4b069e4c7bf4
Peter Xu <soccerdojo@bj-source-server.sportsdata.cn>
2016-05-06 13:30:51 +0800

    refine run_config.sh

--- run_config.sh
+++ run_config.sh
@@ -2,15 +2,14 @@
 
 if [ $# -eq 1 ]; then 
     if [ "$1" = "check" ]; then
-        libpkgs="make automake autoconf libtool"
+        pkgs="libsqlite3-0 liburcu1 libibverbs1 libssl1.0.0 libxml2 libaio1 libacl1 libcmocka0 librdmacm1"
+        pkgs="$pkgs sqlite3 uuid python-simplejson python-ctypeslib libreadline6"
+        sudo apt-get install -y $libpkgs || exit 1
+
+        pkgs="flex bison make automake autoconf libtool pkg-config"
         sudo apt-get install -y $pkgs || exit 1
 
-        libpkgs="libsqlite3-0 liburcu1 libibverbs1 libssl1.0.0 libxml2 libaio1 libacl1 libcmocka0"
-        libpkgs="$libpkgs librdmacm1"
-        libpkgs="$libpkgs sqlite3 uuid python-simplejson python-ctypeslib libreadline6"
-
-        pkgs="flex bison make automake libtool pkg-config"
-        pkgs="$pkgs libsqlite3-dev python-dev liburcu-dev libibverbs-dev libreadline-dev"
+        pkgs="libsqlite3-dev python-dev liburcu-dev libibverbs-dev libreadline-dev"
         pkgs="$pkgs libssl-dev libxml2-dev libaio-dev libacl1-dev uuid-dev libcmocka-dev"
         pkgs="$pkgs librdmacm-dev"
         pkgs="$libpkgs $pkgs"
@@ -22,5 +21,4 @@ fi
 
 [ ! -f ./configure ] && sh autogetn.sh
 
-opts="--prefix=/usr"
-./configure $opts
+./configure
